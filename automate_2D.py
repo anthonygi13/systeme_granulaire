05c5354f37e23e167100129d45829fd7d87f637c
Sebastiao Clement <csebasti@pxe.cbp.ens-lyon.fr>
2020-03-11 14:45:14 +0100

    yes

--- automate_2D.py
+++ automate_2D.py
@@ -5,6 +5,15 @@
 # Created by Anthony Giraudo and Clement Sebastao the 05/02/2020
 
 """
+Ce code permet de creer un automate cellulaire simulant la chute de grains de sable.
+Les regles appliquees ici sont les suivantes :
+    - si un grain n'a pas de grain ou de surface en dessous de lui, il tombe
+    - si un grain est a un sommet avec un voisin a droite mais pas a gauche, il tombe a gauche
+    - si un grain est a un sommet avec un voisin a gauche mais pas a droite, il tombe a droite
+    - si un grain est a un sommet et n'a pas de voisins, il tombe aleatoirement a gauche ou a droite avec
+    la meme probabilite
+Notre systeme est represente dans un array de valeur, le vide est represente par 0, les grains par 1, 
+les surfaces par 2, et les bords agissent comme des surfaces.
 """
 
 # Modules
@@ -16,11 +25,13 @@ from matplotlib import animation
 
 # Functions
 
-def initialisation_automate(n=100, p=0.3):
-    return np.random.binomial(1, p, (n, n))
-
-
+"Fonction recursive permettant de tester si la case verifiee est le sommet d'une colonne de sable ou non."
 def check_sommet(grille, x, y):
+    """
+    :param grille: array, espace de l'automate cellulaire
+    :param x: integer, position en x de la case à verifier
+    :param y: integer, position en y de la case à verifier
+    """
     if x+1 == grille.shape[0] or grille[x+1, y] == 2:
         return True
     elif grille[x+1, y] == 0:
@@ -28,8 +39,11 @@ def check_sommet(grille, x, y):
     elif grille[x+1, y] == 1:
         return check_sommet(grille, x+1, y)
 
-
+"Fonction permettant de recuperer la liste des sommets des colonnes de sable dans notre grille."
 def get_sommets(grille):
+    """
+    :param grille: array, espace de l'automate cellulaire
+    """
     sommets = grille == 1
     sommets[1:, :] *= (grille[:-1, :] == 0)
     indices = np.argwhere(sommets)
@@ -39,20 +53,29 @@ def get_sommets(grille):
                 sommets[x, y] = False
     return sommets
 
-
+"Fonction actualisant notre automate cellulaire d'un pas de temps."
 def etape(grille):
-
-    # FIXME: une tour qui fait toute la hauteur ne s'ecroule pas
-
+    """
+    :param grille: array, espace de l'automate cellulaire
+    """
+    
+    
     old_grille = np.array(grille, copy=True)
-
+    
+    """
+    On commence par faire chuter tous les grains de sable sous lesquelles il n'y a pas de grain.
+    """
     masque_chute = np.zeros(old_grille.shape, dtype=bool)
     masque_chute[:-1, :] = old_grille[1:, :] == 0
     masque_chute *= old_grille == 1
 
     grille[masque_chute] = 0
     grille[1:, :][masque_chute[:-1, :]] = 1
-
+    
+    """
+    On met en place les differents masques nous renseignant sur la condition de notre grain
+    au sommet, d'abord pour savoir si il a un voisin a gauche, droite, en bas a droite ou en bas a gauche. 
+    """
     #sommets = old_grille.shape[0] - np.argmin(np.flip(old_grille, axis=0), axis=0)
     # x = np.indices(old_grille.shape)[0]
     sommets = get_sommets(old_grille)
@@ -64,17 +87,27 @@ def etape(grille):
     pas_voisin_bas_droite[:-1, :-1] = old_grille[1:, 1:] == 0
     pas_voisin_bas_gauche = np.zeros(old_grille.shape, dtype=bool)
     pas_voisin_bas_gauche[:-1, 1:] = old_grille[1:, :-1] == 0
+    
+    """
+    On verifie que nos grains au sommet n'aura pas a cause de la chute d'autres grains des voisins supplementaires.
+    """
     futur_voisin_bas_droite = np.zeros(old_grille.shape, dtype=bool)
     futur_voisin_bas_droite[:-1, :-1] = masque_chute[1:, 1:]
     futur_voisin_bas_gauche = np.zeros(old_grille.shape, dtype=bool)
     futur_voisin_bas_gauche[:-1, 1:] = masque_chute[1:, :-1]
-
+    
+    """
+    On combine nos masques pour avoir un ensemble de conditions sur des masques complets.
+    """
     masque_droite = sommets * pas_voisin_droite * pas_voisin_bas_droite * np.logical_not(futur_voisin_bas_droite)
     masque_gauche = sommets * pas_voisin_gauche * pas_voisin_bas_gauche * np.logical_not(futur_voisin_bas_gauche)
     masque_gauche_pas_droite = masque_gauche * np.logical_not(masque_droite)
     masque_droite_pas_gauche = masque_droite * np.logical_not(masque_gauche)
     masque_droite_gauche = masque_droite * masque_gauche
-
+    
+    """
+    On utilise nos masques complets a notre grille pour l'actualiser.
+    """
     grille[masque_droite + masque_gauche] = 0
 
     rand = np.random.binomial(1, 0.5, old_grille.shape).astype(bool)
@@ -84,8 +117,13 @@ def etape(grille):
     grille[1:, 1:][masque_droite_pas_gauche[:-1, :-1]] = 1
     grille[1:, :-1][masque_gauche_pas_droite[:-1, 1:]] = 1
 
-
+"Fonction qui repete en boucle l'etape de notre automate cellulaire."
 def animate(i, grille, plot):
+    """
+    :param i: integer, nombre de fois que l'on repete l'etape
+    :param grille: array, espace de notre automate cellulaire
+    :param plot: plot de notre automate cellulaire
+    """
     etape(grille)
     plot.set_array(grille)
     return plot,
@@ -94,18 +132,15 @@ def animate(i, grille, plot):
 # Main
 
 if __name__ == "__main__":
-    #grille = initialisation_automate()
-    grille = np.array([[0, 1, 0, 0], [0, 1, 1, 0], [0, 1, 0, 0], [0, 1, 0, 0]])
-    """
+    
     n = 100
     grille = np.zeros((n, n))
-    grille[:50, 0] = 1
+    grille[:49, 0] = 1
     for i in range(20):
         grille[50+i, i] = 2
     grille[80, 18:23] = 2
-    """
-    #grille[50:, 40:60] = np.ones((50, 20))
+
     fig = plt.figure()
     plot = plt.imshow(grille)
-    anim = animation.FuncAnimation(fig, animate, init_func=lambda: None, frames=1000, interval=1000, fargs=(grille, plot), repeat=False)
+    anim = animation.FuncAnimation(fig, animate, init_func=lambda: None, frames=1000, interval=100, fargs=(grille, plot), repeat=False)
     plt.show()
